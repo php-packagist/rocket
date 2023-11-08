bf174337379913e4e78bbf4f737686c681855829
Flc゛ <four_leaf_clover@foxmail.com>
2023-11-08 13:45:25 +0800

    feat(fail): Added code and message

--- example/index.php
+++ example/index.php
@@ -5,8 +5,17 @@ require __DIR__ . '/Rpc.php';
 
 $rpc = new Rpc();
 
+//try {
+//    print_r($rpc->hello(1)->withThrow()->json());
+//} catch (\PhpPackagist\Rocket\Exception\FailedException $e) {
+//    var_dump($e->getResponse()->json());
+//}
+
 try {
-    print_r($rpc->hello(1)->withThrow()->json());
+    print_r($rpc->getToken('facebook', '9faf3c1e-7c94-4573-a248-62e149dcbe87', 'AQBrdgmJVTFT7BuDEcHPFDycezsf_QTN_IuMI2eQiOmWWYo_LveRYa1tgYbWkugZYDGvMUlhScgP7tWariNRTMfkd4Iec9fLBASnIJF-EOwkoxC4HYQV7hhlN8fU0smmit4sy2CRHz5nUk9DzVC6qS6w-wE-NLS9zJnPV2BVg8C2W0j5BdgQ-Siz6fXEV9chJX5QDjEked-9cRkwXK5Gv497SKX8TocI4HoPvW9WHf3oK-SVEDZ1MLql_OkYzuAEHo8-SSx1WrDkZ6Db19GvOh6GnhbPsoGmSKm2IJF5pHI2ptLzKsKRJsTijhw-tgJWV5q605_A9EZ00fpX1jDGSQjXy2IgHGybIvElZCVB1ucrrzMkFoAD7FCtq1UtbqgObxM')->withThrow()->json());
 } catch (\PhpPackagist\Rocket\Exception\FailedException $e) {
-    var_dump($e->getResponse()->json());
+    print_r($e->json());
+    print_r($e->getCode().PHP_EOL);
+    print_r($e->getMessage().PHP_EOL);
+    print_r($e->getReason().PHP_EOL);
 }
--- src/Exception/FailedException.php
+++ src/Exception/FailedException.php
@@ -2,17 +2,82 @@
 
 namespace PhpPackagist\Rocket\Exception;
 
+use PhpPackagist\Rocket\Response;
+
+/**
+ * @mixin Response
+ */
 class FailedException extends \Exception
 {
+    /*
+     * @var Response
+     */
     protected $response;
 
+    /**
+     * @var string
+     */
+    protected $reason;
+
+    /**
+     * @param $response
+     * @return void
+     */
     public function setResponse($response)
     {
         $this->response = $response;
+
+        if ($this->response instanceof Response) {
+            $this->setupResponse();
+        }
     }
 
+    /**
+     * @return Response
+     */
     public function getResponse()
     {
         return $this->response;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    /**
+     * Setup the response object.
+     *
+     * @return void
+     */
+    protected function setupResponse()
+    {
+        $json = $this->response->json();
+
+        if (!is_array($json)) {
+            return ;
+        }
+
+        $this->code = $json['code'] ?: $this->code;
+        $this->message = $json['message'] ?: $this->message;
+        $this->reason = $json['reason'] ?: $this->reason;
+    }
+
+    /**
+     * Call a method on the underlying response.
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        if ($this->response instanceof Response && method_exists($this->response, $method)) {
+            return call_user_func_array(array($this->response, $method), $args);
+        }
+
+        throw new \BadMethodCallException("Call to undefined method [{$method}]");
     }
 }
